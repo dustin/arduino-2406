b8130a133ed856f26db57a97974005e8a0515e2d
Dustin Sallings <dustin@spy.net>
2009-11-27 01:51:56 -0800

    Initial version of my 2406 lib.

---
+++ DS2406.cpp
@@ -0,0 +1,45 @@
+#include <DS2406.h>
+
+OneWireSwitch::OneWireSwitch(OneWire *b, uint8_t *addr) {
+    bus = b;
+    for(int i = 0; i<8; i++) {
+        address[i] = addr[i];
+    }
+}
+
+bool OneWireSwitch::getSwitchState(int port) {
+    uint8_t status[10];
+    readStatus(status);
+    return (status[7] & port) == 0;
+}
+
+void OneWireSwitch::setSwitchState(bool pio_a, bool pio_b) {
+    uint8_t state = (pio_a << 5) | (pio_b << 6) | 0xf;
+    bus->reset();
+    bus->select(address);
+    bus->write(DS2406_WRITE_STATUS);
+    bus->write(0x07);
+    bus->write(0);
+
+    bus->write(state);
+    // Read the CRC data
+    for (int i = 0; i < 6; i++) {
+        bus->read();
+    }
+    // Write the status back.
+    bus->write(0xFF,1);
+}
+
+void OneWireSwitch::readStatus(uint8_t *buffer) {
+    bus->reset();
+    bus->select(address);
+    bus->write(DS2406_READ_STATUS, 1);
+    bus->write(0, 1);
+    bus->write(0, 1);
+
+    for(int i = 0; i<10; i++) {
+        buffer[i] = bus->read();
+    }
+    bus->reset();
+}
+
---
+++ DS2406.h
@@ -0,0 +1,35 @@
+#include <OneWire.h>
+
+#include <inttypes.h>
+
+#define DS2406_FAMILY       0x12
+
+#define DS2406_WRITE_STATUS 0x55
+#define DS2406_READ_STATUS  0xaa
+
+#define PIO_A 0x20
+#define PIO_B 0x40
+
+// Represents a single 1wire switch on an MLan.
+class OneWireSwitch {
+
+ public:
+
+    // Construct the OneWireSwitch with the given OneWire bus and address.
+    // addr must be 8 bytes.
+    OneWireSwitch(OneWire *b, uint8_t *addr);
+
+    // Get the switch state.
+    // An optional argument indicates *which* switch state in the case
+    // where you have a TSOC package with two ports.
+    bool getSwitchState(int port = PIO_A);
+
+    // Set the switch state(s).
+    void setSwitchState(bool pio_a, bool pio_b = false);
+
+ private:
+    OneWire *bus;
+    uint8_t address[8];
+
+    void readStatus(uint8_t *buffer);
+};
